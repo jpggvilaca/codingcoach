43fd1461ea0524d15dc00e46782a84dfdded3b19
analogy47 <actweed47@gmail.com>
2019-06-03 14:29:59 -0400

    add axios and set-up folder structure

--- movie-browser/src/App.js
+++ movie-browser/src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MovieList from './components/MovieList';
+import axios from 'axios';
 
-const App = () => {
-  return <div>Movie Browser</div>;
-};
+const URL = 'https://ghibliapi.herokuapp.com/films';
+
+class App extends Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(URL)
+      .then(res => {
+        this.setState({
+          movies: res.data,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <h1>Movie Browser</h1>
+        <MovieList />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ movie-browser/src/components/MovieList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieList = () => {
+  return (
+    <div>
+      <h3>Trending Movies</h3>
+      <h3>All Movies</h3>
+    </div>
+  );
+};
+
+export default MovieList;
